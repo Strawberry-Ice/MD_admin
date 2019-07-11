f670e1470728f6e18f07a5b9e0d733ef3b0493c4
Strawberry-Ice <renyaxing@outlook.com>
2019-07-11 17:11:08 +0800

    获取日常下单用户

--- meiduo_mall/apps/meiduo_admin/views/home_views.py
+++ meiduo_mall/apps/meiduo_admin/views/home_views.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from users.models import User
 from django.utils import timezone
 
 from meiduo_admin.utils import obtain_zero_shanghai
+from users.models import User
+from orders.models import OrderInfo
 
 
 class HomeViewSet(ViewSet):
@@ -52,6 +53,40 @@ class HomeViewSet(ViewSet):
         count = User.objects.filter(last_login__gte=date_zero_shanghai).count()
 
         # 3.统计数据,构建数据返回
+        return Response({
+            'count': count,
+            'date': date_zero_shanghai
+        })
+
+    # 日常下单用户
+    # GET
+    # statistical/day_orders/
+    @action(methods=['get'], detail=False)
+    def day_orders(self, request):
+        # 1.获取当日零时
+        date_zero_shanghai = obtain_zero_shanghai()
+
+        # 2.统计日下单用户
+
+        # 2.1 以从表查询
+        # 2.1.1 过滤出当日下单的订单
+        # order_queryset = OrderInfo.objects.filter(create_time__gte=date_zero_shanghai)
+
+        # 2.1.2 根据订单,统计出用户
+        # user_list = []
+        # for order in order_queryset:
+            # if order.user not in user_list:
+            #     user_list.append(order.user)
+            # user_list.append(order.user)
+
+        # 2.1.3 用户去重
+        # count = len(user_list)
+        # count = len(set(user_list))
+
+        # 2.2 以主表查询
+        user_queryset = User.objects.filter(orders__create_time__gte=date_zero_shanghai)
+        count = len(set(user_queryset))
+        # 3.构建数据返回
         return Response({
             'count': count,
             'date': date_zero_shanghai
